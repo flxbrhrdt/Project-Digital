e66c24c2d7b9db4b224e594d7a1996bf6ce00f43
Felix Eberhardt <feberhardt@olin.edu>
2017-11-09 12:14:00 -0500

    simulate keypress with pynput (1 to 5)

--- bot.py
+++ bot.py
@@ -1,7 +1,11 @@
 import pygame
 import sys
+
 pygame.init()
 
+#simulate keypress
+from pynput.keyboard import Key, Controller
+keyboard = Controller()
 
 c1location = (1,1)
 c2location = (2,1)
@@ -21,3 +25,29 @@ while not done:
             print("column1")
             done = True
     pygame.event.pump()
+
+def choose_option():
+    return 4
+
+def fake_keydown():
+    """ creates a key.down output for the game script """
+    if choose_option() == 1:
+        simulate_keypress('1')
+    elif choose_option() == 2:
+        simulate_keypress('2')
+    elif choose_option() == 3:
+        simulate_keypress('3')
+    elif choose_option() == 4:
+        simulate_keypress('4')
+    if choose_option() == 5:
+        simulate_keypress('5')
+    else:
+        simulate_keypress('3')
+    pass
+
+def simulate_keypress(keypress):
+    """simulates keypress """
+    keyboard.press(keypress)
+    keyboard.release(keypress)
+
+fake_keydown()
