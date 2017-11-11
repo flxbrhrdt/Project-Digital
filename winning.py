fb7324c03341c3e87371ec2d65027fbf55055ca9
sokuno222 <siena.okuno@students.olin.edu>
2017-11-11 10:56:25 -0500

    Added scoring for vertical and horiztonal

--- winning.py
+++ winning.py
@@ -14,15 +14,46 @@ def winning(matrix, connect):
     #vertical
     for y in range(columns):
         for x in range(rows-connect+1):
-            if matrix[x+1,y] == matrix[x+1,y] == matrix[x+2,y] and matrix[x,y] != 0:     #add another == for Connect 4
+            if matrix[x,y] == matrix[x+1,y] == matrix[x+2,y] and matrix[x,y] != 0:     #add another == for Connect 4
                 return True, matrix[x,y]
-
     #diagonal pos
-    
+
     #diagonal neg
     #tie
 
     return False, 0
 
-a = numpy.matrix('1 0 0 0; 0 0 0 1; 1 0 1 0')
-print(winning(a,3))
+def scoring(matrix, connect):
+    rows = matrix.shape[0]
+    columns = matrix.shape[1]
+    a = winning(matrix, connect)[0]
+    score = 0
+    if a == False:
+        #horizontal
+        for x in range(rows):
+            for y in range(columns-1):
+                if matrix[x,y] == matrix[x,y+1] and matrix[x,y] != 0:     #add another == for Connect 4
+                    score += 3
+                    print('2x')
+            if score == 0:
+                for y in range(columns):
+                    if matrix[x,y] != 0:
+                        score += 1
+                        print('1x')
+        #vertical
+        for y in range(columns):
+            for x in range(rows-1):
+                if matrix[x,y] == matrix[x+1,y] and matrix[x,y] != 0:     #add another == for Connect 4
+                    score += 3
+                    print("2y")
+            if score == 0:
+                for x in range(rows):
+                        if matrix[x,y] != 0:
+                            score += 1
+                            print("1y")
+    elif a == True:
+        score = 10
+    return score
+
+a = numpy.matrix('0 0 1 0; 0 0 1 0; 0 0 1 0')
+print(scoring(a,3))
