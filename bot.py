4852b5978039bd103eec67441001764540c95bba
sokuno222 <siena.okuno@students.olin.edu>
2017-11-13 11:33:25 -0500

    Started implementing minimax

--- bot.py
+++ bot.py
@@ -1,30 +1,26 @@
 import pygame
 import sys
-
-pygame.init()
-
-#simulate keypress
 from pynput.keyboard import Key, Controller
+from winning.py import *
 keyboard = Controller()
 
-c1location = (1,1)
-c2location = (2,1)
-c3location = (3,1)
-c4location = (4,2)
-c5location = (5,1)
-MOUSE = pygame.USEREVENT + 1
-column1 = pygame.event.Event(MOUSE, location=c1location)
-pygame.event.post(column1)
-
-
-pygame.event.get
-done = False
-while not done:
-    for event in pygame.event.get():
-        if event.type == MOUSE:
-            print("column1")
-            done = True
-    pygame.event.pump()
+pygame.init()
+#c1location = (1,1)
+#c2location = (2,1)
+#c3location = (3,1)
+#c4location = (4,2)
+#c5location = (5,1)
+#MOUSE = pygame.USEREVENT + 1
+#column1 = pygame.event.Event(MOUSE, location=c1location)
+#pygame.event.post(column1)
+#pygame.event.get
+#done = False
+#while not done:
+#    for event in pygame.event.get():
+#        if event.type == MOUSE:
+#            print("column1")
+#            done = True
+#    pygame.event.pump()
 
 
 # #Keyboard input for bot, to control it before completing calculations (if we want to implement this instead of mouse click). Can be completed after structure of matrix is set.
@@ -40,6 +36,29 @@ while not done:
 #                 #place in fourth column of matrix, set 0 in lowest row within column to 2
 #             if event.key == K_5:
 #                 #place in fifth column of matrix, set 0 in lowest row within column to 2
+def minimax(node, depth, myTurn, matrix, connect):
+    """Evaluation of scores and nodes to pick the best move.
+    Takes depth, boolean of myTurn, matrix of the playing field, and # to connect.
+    Returns an """
+    if depth == 0 or node is terminal
+        return scoring(matrix, connect)
+    if myTurn:
+        bestValue = 
+    else:
+
+05         bestValue := −∞
+06         for each child of node
+07             v := minimax(child, depth − 1, FALSE)
+08             bestValue := max(bestValue, v)
+09         return bestValue
+
+10     else    (* minimizing player *)
+11         bestValue := +∞
+12         for each child of node
+13             v := minimax(child, depth − 1, TRUE)
+14             bestValue := min(bestValue, v)
+15         return bestValue
+
 def choose_option():
     """choose the the best option """
     best_option = 2
