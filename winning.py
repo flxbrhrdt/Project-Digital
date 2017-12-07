cfae074af97b7497e92a54ff51dea508a5832a8a
sokuno222 <siena.okuno@students.olin.edu>
2017-12-07 11:20:15 -0500

    Updated winning() for connect 4 use

--- winning.py
+++ winning.py
@@ -3,18 +3,18 @@
 #Determines if game is won and by who when given a matrix
 import numpy
 
-def winning(matrix, connect=3):
+def winning(matrix, connect=4):
     rows = matrix.shape[0]
     columns = matrix.shape[1]
     #horizontal
     for x in range(rows):
         for y in range(columns-connect+1):
-            if matrix[x,y] == matrix[x,y+1] == matrix[x,y+2] and matrix[x,y] != 0:     #add another == for Connect 4
+            if matrix[x,y] == matrix[x,y+1] == matrix[x,y+2] == matrix[x,y+3] and matrix[x,y] != 0:
                 return True, matrix[x,y]
     #vertical
     for y in range(columns):
         for x in range(rows-connect+1):
-            if matrix[x,y] == matrix[x+1,y] == matrix[x+2,y] and matrix[x,y] != 0:     #add another == for Connect 4
+            if matrix[x,y] == matrix[x+1,y] == matrix[x+2,y] == matrix[x+3,y] and matrix[x,y] != 0:
                 return True, matrix[x,y]
     #determines which side is longer for diagonal search
     if rows>columns:
@@ -26,14 +26,14 @@ def winning(matrix, connect=3):
     for x in range(longer-1):
         a = numpy.diagonal(arrayM,x-(rows-connect+1))
         for i in range(len(a)-connect+1):
-            if a[i] == a[i++1] == a[i+2] and a[i] !=0:
+            if a[i] == a[i++1] == a[i+2] ==a[i+3] and a[i] !=0:
                 return True, a[i]
     #reverse diagonal
     arrayM = numpy.asarray(numpy.fliplr(matrix))
     for x in range(longer-1):
         a = numpy.diagonal(arrayM,x-(rows-connect+1))
         for i in range(len(a)-connect+1):
-            if a[i] == a[i++1] == a[i+2] and a[i] !=0:
+            if a[i] == a[i++1] == a[i+2] == a[i+3] and a[i] !=0:
                 return True, a[i]
     #tie
     if numpy.count_nonzero(arrayM) == rows*columns:
